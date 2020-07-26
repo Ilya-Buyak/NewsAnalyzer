47ba496c3cb7c6187c12c53b8f1c6cd36c5956a3
Ilya <Ilya_11_11@mail.ru>
2020-07-26 13:04:23 +0300

    add about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,1 +1,16 @@
 import './style.css'
+
+const copyright = document.querySelector('.footer__copyright');
+copyright.textContent = `© ${new Date().getFullYear()} Supersite`
+// на следующем этапе перенесется в отдельный js файл
+function sliderInitialization() {
+  let Flickity = require('flickity');
+  let flkty = new Flickity( '.slider', {
+    freeScroll: true,
+    wrapAround:  true,
+    initialIndex: 6,
+    groupCells: true
+  });
+}
+
+sliderInitialization()
